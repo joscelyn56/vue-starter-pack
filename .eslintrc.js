760bc36a7ac36b5b4b571a2c59fd52d09a13ff2c
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2019-07-22 00:20:35 +0100

    Added eslint config and update index.html

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "sourceType": "module",
+    "allowImportExportEverywhere": false
+  },
+  extends: [
+    'plugin:vue/recommended'
+  ]
+}
